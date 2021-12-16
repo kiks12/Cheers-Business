48ad95f14b4a65237c5b1758407a22ebcc2e6ce3
Francis James Tolentino <francistolentino1107@gmail.com>
2021-12-16 19:57:48 +0800

    grid

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,19 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 
 const Home: NextPage = () => {
-	return <div className="bg-yellow-50 w-screen h-screen">DASHBOARD</div>;
+	return (
+		<>
+			<Head>
+				<title>Cheers Dashboard</title>
+			</Head>
+			<main className="h-screen w-screen grid grid-cols-6 gap-2 border">
+				<div className="h-100">asdfsaf</div>
+				<div className="col-span-4 h-100">asdfsaf</div>
+				<div className="h-100">asdfsaf</div>
+			</main>
+		</>
+	);
 };
 
 export default Home;

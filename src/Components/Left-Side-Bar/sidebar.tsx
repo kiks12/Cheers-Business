56fa4e1c67b268aed4df0fa4cb8e67fb0c080e83
Francis James Tolentino <francistolentino1107@gmail.com>
2021-12-19 10:52:22 +0800

    sidebar restyle

--- src/Components/Left-Side-Bar/sidebar.tsx
+++ src/Components/Left-Side-Bar/sidebar.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 
 const Sidebar: React.FC = () => {
-    return (
-        <div>
-            <nav>
-                <ul>
-                    <li>Home</li>
-                    <li>Reservations</li>
-                    <li>Settings</li>
-                </ul>
-            </nav>
-        </div>
-    )
-}
+	return (
+		<div>
+			<nav className="flex flex-col items-center justify-center">
+				<h1 className="py-5 text-lg">{"{Bar Name}"}</h1>
+				<ul className="flex flex-col w-full">
+					<li className="h-10 flex items-center justify-center hover:bg-gray-100 cursor-pointer transition-all">Home</li>
+					<li className="h-10 flex items-center justify-center hover:bg-gray-100 cursor-pointer transition-all">Reservations</li>
+					<li className="h-10 flex items-center justify-center hover:bg-gray-100 cursor-pointer transition-all">Settings</li>
+				</ul>
+			</nav>
+		</div>
+	);
+};
 
 export default Sidebar;
